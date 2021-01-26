617cd630e74d2892161d386742a5c216a8dacbf8
Rugz007 <dsweb007@gmail.com>
2021-01-26 18:40:05 +0000

    Made Enquiry Frontend

---
+++ frontend/src/components/HomePage/InquiryForm.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Button, Card, Divider, Form, Input, Select } from 'antd';
+import ReCAPTCHA from 'react-google-recaptcha';
+const { Option } = Select;
+
+export const InquiryForm: React.FC = () => {
+    const onFinish = () => {
+        console.log("Clicked Finished");
+    };
+    const onCaptchaChange = (value: string | null) => {
+        console.log(value);
+    };
+    return (
+        <Card bordered={false} style={{ height: '100%', borderRadius: '17px' ,marginLeft:'3%'}}>
+            <h1 style={{ fontSize: '40px', marginBottom: '0' }}><b>Enquiry Form</b></h1>
+            <h3>Put in your details and we will be in touch with you soon.</h3>
+            <Divider />
+            <Form
+                name="enquiry"
+                onFinish={onFinish}
+                scrollToFirstError
+                style={{ textAlign: 'left' }}
+            >
+                <h2><b>Full Name:</b></h2>
+                <Form.Item name="fullname" >
+                    <Input placeholder={'Enter your Full Name.'} />
+                </Form.Item>
+                <h2><b>Email ID:</b></h2>
+                <Form.Item name="email">
+                    <Input placeholder={'Enter your Email ID'} />
+                </Form.Item>
+                <h2><b>Phone Number</b></h2>
+                <Form.Item name="number">
+                    <Input placeholder={'Enter your Phone Number.'} />
+                </Form.Item>
+                <h2><b>Which class are you enquiring for?</b></h2>
+                <Form.Item name="class">
+                    <Select placeholder={'Select your class.'}>
+                        <Option value="VII">
+                            VII
+                        </Option>
+                        <Option value="VIII">
+                            VIII
+                        </Option>
+                        <Option value="IX">
+                            IX
+                        </Option>
+                        <Option value="X">
+                            X
+                        </Option>
+                        <Option value="XI">
+                            XI
+                        </Option>
+                        <Option value="XII">
+                            XII
+                        </Option>
+                    </Select>
+                </Form.Item>
+                <ReCAPTCHA sitekey="6LfhIz0aAAAAAORE5vd7fw2XQjYKfVsFjHyB5EGQ" theme='dark' onChange={onCaptchaChange}></ReCAPTCHA>
+                <Form.Item name='button'>
+                    <Button type='primary' size='large' style={{ width: '100%', margin: 'auto' }}>Submit</Button>
+                </Form.Item>
+            </Form>
+        </Card>
+    );
+}
--- frontend/src/views/HomePage/HomePage.tsx
+++ frontend/src/views/HomePage/HomePage.tsx
@@ -1,11 +1,26 @@
+import {  Col, Row } from 'antd';
 import Layout, { Content } from 'antd/lib/layout/layout';
 import React from 'react'
+import { InquiryForm } from '../../components/HomePage/InquiryForm';
 
 
 export const HomePage: React.FC = () => {
     return (
         <Layout>
-            <Content></Content>
+            <Content style={{backgroundColor:'#000b14'}}>
+                <Row >
+                    <Col span={4} />
+                    <Col span={9} style={{paddingTop:'5%'}}>
+                        <h1 style={{fontSize:'28px'}}><b>Want to perform exceptionally in the upcoming competitive exams?</b></h1>
+                        <h1 style={{fontSize:'22px'}} ><b><b>Join us today!</b></b></h1>
+                        <img src='study.svg' alt='study' style={{width:'50%'}}/>
+                    </Col>
+                    <Col span={7} style={{padding:'.4%',paddingTop:'4%'}}>
+                        <InquiryForm />
+                    </Col>
+                    <Col span={4} />
+                </Row>
+            </Content>
         </Layout>
     );
 }
