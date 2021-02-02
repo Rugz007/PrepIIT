503028f9b9fefaab5db8efb5618686d604a1f5c6
Rugz007 <dsweb007@gmail.com>
2021-02-02 06:04:38 +0000

    Updated SVG Fixed and Updated Footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,26 +1,34 @@
-import { Divider, Row, Col } from 'antd'
-import Layout, { Footer as AntDFooter } from 'antd/lib/layout/layout';
+import { Row, Col } from 'antd'
 import React from 'react'
 
 export const Footer: React.FC = () => {
     return (
-        <Layout>
-            <AntDFooter style={{ backgroundColor: '#2a323c' }}>
-                <Divider />
-                <Row>
-                    <Col span={3} />
-                    <Col span={6}>
-                        <h1>Check us out on Social Media</h1>
-                    </Col>
-                    <Col span={6}>
-                        <h1>Quick Links</h1>
-                    </Col>
-                    <Col span={6}>
-                        <h1>Check us out on Social Media</h1>
-                    </Col>
-                    <Col span={3} />
-                </Row>
-            </AntDFooter>
-        </Layout>
+        <Row style={{ backgroundColor: '#FFFFFF', color: 'black', paddingTop: '1%' }}>
+            <Col span={3} />
+            <Col span={6}>
+                <h1 style={{ color: 'black', marginBottom: '0' }}>Check us out on Social Media</h1>
+                <div >
+                    <img alt='facebook' src="https://img.icons8.com/fluent/48/000000/facebook-new.png" />
+                    <img alt='instagram' src="https://img.icons8.com/fluent/48/000000/instagram-new.png" />
+                    <img alt='linkedin' src="https://img.icons8.com/fluent/48/000000/linkedin.png" />
+                    <img alt='youtube' src="https://img.icons8.com/fluent/48/000000/youtube-play.png" />
+                </div>
+            </Col>
+            <Col span={6}>
+                <h1 style={{ color: 'black' }}>Quick Links</h1>
+                <ul>
+                    <li>
+                        <a>Courses</a>
+                    </li>
+                    <li>
+                        <a>Resources</a>
+                    </li>
+                </ul>
+            </Col>
+            <Col span={6}>
+                <h1 style={{ color: 'black' }}>Contact</h1>
+            </Col>
+            <Col span={3} />
+        </Row>
     );
 }
