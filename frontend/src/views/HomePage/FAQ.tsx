5b9bbc906808f81fcbf72c6e08d12537781fea22
Rugz007 <dsweb007@gmail.com>
2021-02-04 19:12:03 +0000

    Basic Course done and working on FAQ

--- frontend/src/views/HomePage/FAQ.tsx
+++ frontend/src/views/HomePage/FAQ.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import { Collapse, Card, Row, Col, Divider } from 'antd';
+import Layout, { Content } from 'antd/lib/layout/layout';
+const { Panel } = Collapse;
 export const FAQ: React.FC = () => {
     return (
-        <>
+        <Layout>
+            <Content style={{ backgroundColor: '#2a323c' }}>
+                <Row>
+                    <h1 style={{ width: '100%', margin: 'auto' }}>Courses we Offer:</h1>
+                </Row>
+                <Row style={{ paddingBottom: '3%' }}>
+                    <Col span={4} />
+                    <Col span={16}>
+                        <h1>Common Student Questions</h1>
+                        <Collapse >
+                            <Panel header="What method of study will guarantee my success?" key={1} >
 
-        </>
+                            </Panel>
+                        </Collapse>
+                        <Divider style={{ marginTop: '0' }} />
+                    </Col>
+                    <Col span={4} />
+                </Row>
+            </Content >
+        </Layout >
     );
 }
