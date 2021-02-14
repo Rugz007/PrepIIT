409afaeb19e2a75539bb57fd198951cd45d2a8a3
rajatmaheshwari2512 <mrajat67@yahoo.com>
2021-02-14 09:58:50 +0000

    Admin Read and Delete Questions Complete

--- backend/routes/adminRouter.js
+++ backend/routes/adminRouter.js
@@ -24,6 +24,71 @@ router
         console.log("DB Error");
         res.status(500).json({ success: false });
       });
+  })
+  .get("/question", (req, res, next) => {
+    db.query("SELECT * FROM questions")
+      .then((resp) => res.status(200).json(resp.rows))
+      .catch((err) => {
+        console.log("DB Error");
+        res.status(500).json({ success: false });
+      });
+  })
+  .post("/question", (req, res, next) => {
+    console.log(req.body);
+    const {
+      statement,
+      latex,
+      img_path,
+      type,
+      subject,
+      topic,
+      subtopic,
+      level,
+      archive,
+      is_reported,
+    } = req.body;
+    db.query(
+      "INSERT INTO questions VALUES (DEFAULT,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10)",
+      [
+        statement,
+        latex,
+        img_path,
+        type,
+        subject,
+        topic,
+        subtopic,
+        level,
+        archive,
+        is_reported,
+      ]
+    )
+      .then((resp) => {
+        console.log("Inserted Successfully");
+        res.status(201).json({ success: true });
+      })
+      .catch((err) => {
+        console.log("DB Error");
+        res.status(500).json({ success: false });
+      });
+  })
+  .delete("/question", (req, res, next) => {
+    const qid = req.body.qid;
+    if (qid) {
+      db.query("DELETE FROM questions WHERE qid=$1", [qid])
+        .then((resp) => {
+          console.log("Deleted Successfully");
+          res.status(201).json({ success: true });
+        })
+        .catch((err) => {
+          console.log("DB Error");
+          res.status(500).json({ success: false });
+        });
+    } else {
+      res.json({
+        success: false,
+        errmess: "No Question ID Found",
+      });
+    }
   });
 
 module.exports = router;
--- frontend/src/components/Dashboard/Questions/QuestionBank.tsx
+++ frontend/src/components/Dashboard/Questions/QuestionBank.tsx
@@ -1,75 +1,123 @@
 import { Card, Table, Button } from "antd";
+import Item from "antd/lib/list/Item";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 interface QuestionInterface {
-    qid:number,
-    statement:string,
-    latex?:any,
-    img_path?:string,
-    type:string,
-    subject:string,
-    topic:string,
-    subtopic?:string,
-    level:string,
-    archive?:string,
-    is_reported:boolean,
+  qid: number;
+  statement: string;
+  latex?: any;
+  img_path?: string;
+  type: string;
+  subject: string;
+  topic: string;
+  subtopic?: string;
+  level: string;
+  archive?: string;
+  is_reported: boolean;
 }
 
 export const QuestionBank: React.FC = () => {
-    useEffect(()=>
-    {
-        fetchBankDetails();
-    },[]);
-    
-    const fetchBankDetails = () =>
-    {
-        //TODO: Fetch Total Questions and other details for nice purposes. 
+  const [questions, setQuestions] = useState<QuestionInterface[] | undefined>(
+    []
+  );
+  useEffect(() => {
+    fetchBankDetails();
+  }, []);
+
+  const fetchBankDetails = async () => {
+    try {
+      const res = await axios.get("http://localhost:3000/admin/question", {
+        headers: {
+          authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      console.log(res.data);
+      setQuestions(res.data);
+    } catch (e) {
+      console.log("Questions not Loaded");
     }
-    const columns = [
+  };
+  const deleteQuestion = async (
+    qid: Number,
+    questions: any[] | undefined,
+    setQuestions: {
+      (value: React.SetStateAction<QuestionInterface[] | undefined>): void;
+      (arg0: any): void;
+    }
+  ) => {
+    try {
+      const response = await axios.delete(
+        "http://localhost:3000/admin/question",
         {
-          title: "Question ID",
-          dataIndex: "qid",
-          key: "qid",
-        },
-        {
-          title: "Subject",
-          dataIndex: "subject",
-          key: "subject",
-        },
-        {
-          title: "Topic",
-          dataIndex: "topic",
-          key: "topic",
-        },
-        {
-          title: "Type",
-          dataIndex: "type",
-          key: "type",
-        },
-        {
-          title: "Action",
-          dataIndex: "",
-          key: "action",
-          render: (text: any, record: QuestionInterface) => (
-            <>
-              <Button
-                type="primary"
-                danger
-              >
-                Delete
-              </Button>
-            </>
-          ),
-        },
-      ];
-    return (
-        <div style={{ textAlign: "left" }}>
-            <h1 style={{ fontSize: "40px" }}>Question Bank</h1>
-            <Card style={{ textAlign: "left", borderRadius: "10px" }}>
-                <h1 style={{ fontSize: "30px" }}>View Latest Added Questions</h1>
-                <Table columns={columns} style={{ width: "100%" }} />
-            </Card>
-        </div>
-    );
-}
+          headers: {
+            authorization: "Bearer " + localStorage.getItem("token"),
+          },
+          data: {
+            qid: qid,
+          },
+        }
+      );
+      console.log(response);
+      if (questions) {
+        const newState = questions.filter(
+          (item: { qid: Number }) => item.qid !== qid
+        );
+        setQuestions(newState);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const columns = [
+    {
+      title: "Question ID",
+      dataIndex: "qid",
+      key: "qid",
+    },
+    {
+      title: "Subject",
+      dataIndex: "subject",
+      key: "subject",
+    },
+    {
+      title: "Topic",
+      dataIndex: "topic",
+      key: "topic",
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+      key: "type",
+    },
+    {
+      title: "Action",
+      dataIndex: "",
+      key: "action",
+      render: (text: any, record: QuestionInterface) => (
+        <>
+          <Button
+            type="primary"
+            onClick={() => deleteQuestion(record.qid, questions, setQuestions)}
+            danger
+          >
+            Delete
+          </Button>
+        </>
+      ),
+    },
+  ];
+  return (
+    <div style={{ textAlign: "left" }}>
+      <h1 style={{ fontSize: "40px" }}>Question Bank</h1>
+      <Card style={{ textAlign: "left", borderRadius: "10px" }}>
+        <h1 style={{ fontSize: "30px" }}>View Latest Added Questions</h1>
+        <Table
+          columns={columns}
+          dataSource={questions}
+          style={{ width: "100%" }}
+        />
+      </Card>
+    </div>
+  );
+};
