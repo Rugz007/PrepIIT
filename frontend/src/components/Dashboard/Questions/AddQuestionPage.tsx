1e61b0a020706cbc0b6c00b8871e9e44f3a22883
Rugz007 <dsweb007@gmail.com>
2021-02-14 08:19:46 +0000

    Added Upload Questions

--- frontend/src/components/Dashboard/Questions/AddQuestionPage.tsx
+++ frontend/src/components/Dashboard/Questions/AddQuestionPage.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from 'antd'
 import React from 'react'
+import { UploadQuestions } from './UploadQuestions'
 
 
 export const AddQuestionPage: React.FC = () => {
@@ -8,7 +9,7 @@ export const AddQuestionPage: React.FC = () => {
             <h1 style={{ fontSize: "40px" }}>Add Questions</h1>
             <Tabs>
                 <Tabs.TabPane tab='Add new Questions' key='1'>Add Questions</Tabs.TabPane>
-                <Tabs.TabPane tab='Upload Questions' key='2'>Upload Questions</Tabs.TabPane>
+                <Tabs.TabPane tab='Upload Questions' key='2'><UploadQuestions/></Tabs.TabPane>
             </Tabs>
         </div>
     );
