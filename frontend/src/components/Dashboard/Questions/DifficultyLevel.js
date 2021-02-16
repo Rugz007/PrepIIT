d4ea1ef9313b37878f51e854a5cb515a4a0b332b
Rugz007 <dsweb007@gmail.com>
2021-02-16 07:36:10 +0000

    Made Question modal and changed a few things

---
+++ frontend/src/components/Dashboard/Questions/DifficultyLevel.js
@@ -0,0 +1,19 @@
+const Levels = [
+    {
+        name:'Very Easy',
+    },
+    {
+        name:'Easy',
+    },
+    {
+        name:'Medium',
+    },
+    {
+        name:'Hard',
+    },
+    {
+        name:'Very Hard',
+    }
+]
+
+export default Levels;
