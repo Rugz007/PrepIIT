59a8653fba24c0c51f6a304290a750359e270b7c
rajatmaheshwari2512 <mrajat67@yahoo.com>
2021-02-19 15:59:21 +0000

    Completed Edit Questions, and Resolve Reported Questions

--- backend/routes/secureRouter.js
+++ backend/routes/secureRouter.js
@@ -2,10 +2,18 @@ var express = require("express");
 var router = express.Router();
 const userAuth = require("../userAuth/userAuth");
 
+var db = require("../db/index");
+
 router.use(userAuth);
 
-router.get("/test", (req, res, next) => {
-  res.status(200).json({ success: true, message: "You are verified" });
-});
+router
+  .get("/test", (req, res, next) => {
+    res.status(200).json({ success: true, message: "You are verified" });
+  })
+  .post("/test", (req, res, next) => {
+    const mcq = 20;
+    const numerical = 10;
+    res.end();
+  });
 
 module.exports = router;
