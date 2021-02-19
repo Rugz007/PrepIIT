33c28daf662b45d8ef9cf1bd78dbecb9b15737b1
Rugz007 <dsweb007@gmail.com>
2021-02-19 15:49:58 +0000

    Tests Page Component created

---
+++ frontend/src/views/Test/TestsPage.tsx
@@ -0,0 +1,14 @@
+import { Row } from 'antd';
+import React from 'react'
+
+interface TestsPageProps {
+
+}
+
+export const TestsPage: React.FC<TestsPageProps> = () => {
+    return (
+        <Row>
+
+        </Row>
+    );
+}
