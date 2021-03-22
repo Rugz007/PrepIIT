54ecc8a11d7ab7df7a13476b30753a11bc9f187e
rajatmaheshwari2512 <mrajat67@yahoo.com>
2021-03-22 07:28:19 +0000

    Fixed Questions Response

--- backend/testGeneration/allThree.js
+++ backend/testGeneration/allThree.js
@@ -12,124 +12,124 @@ const allThree = (testObject, res) => {
   });
   const mcqQuestions = testObject.mcqdata ? testObject.mcqdata[0] : 0;
   db.query(
-    `SELECT * FROM questions WHERE is_reported=FALSE OR subject='physics' ORDER BY RANDOM() LIMIT ${mcqQuestions}`
+    `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='physics' ORDER BY RANDOM() LIMIT ${mcqQuestions}`
   ).then((resp) => {
     if (resp.rows) phyQues.push(resp.rows);
     const fibQuestions = testObject.fibdata ? testObject.fibdata[0] : 0;
     db.query(
-      `SELECT * FROM questions WHERE is_reported=FALSE OR subject='physics' ORDER BY RANDOM() LIMIT ${fibQuestions}`
+      `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='physics' ORDER BY RANDOM() LIMIT ${fibQuestions}`
     ).then((resp) => {
       if (resp.rows) phyQues.push(resp.rows);
       const assertionQuestions = testObject.assertiondata
         ? testObject.assertiondata[0]
         : 0;
       db.query(
-        `SELECT * FROM questions WHERE is_reported=FALSE OR subject='physics' ORDER BY RANDOM() LIMIT ${assertionQuestions}`
+        `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='physics' ORDER BY RANDOM() LIMIT ${assertionQuestions}`
       ).then((resp) => {
         if (resp.rows) phyQues.push(resp.rows);
         const trueFalseQuestions = testObject.truefalse
           ? testObject.truefalse[0]
           : 0;
         db.query(
-          `SELECT * FROM questions WHERE is_reported=FALSE OR subject='physics' ORDER BY RANDOM() LIMIT ${trueFalseQuestions}`
+          `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='physics' ORDER BY RANDOM() LIMIT ${trueFalseQuestions}`
         ).then((resp) => {
           if (resp.rows) phyQues.push(resp.rows);
           const numericalQuestions = testObject.numerical
             ? testObject.numerical[0]
             : 0;
           db.query(
-            `SELECT * FROM questions WHERE is_reported=FALSE OR subject='physics' ORDER BY RANDOM() LIMIT ${numericalQuestions}`
+            `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='physics' ORDER BY RANDOM() LIMIT ${numericalQuestions}`
           ).then((resp) => {
             if (resp.rows) phyQues.push(resp.rows);
             const matchColumnQuestions = testObject.matchcolumn
               ? testObject.matchcolumn[0]
               : 0;
             db.query(
-              `SELECT * FROM questions WHERE is_reported=FALSE OR subject='physics' ORDER BY RANDOM() LIMIT ${matchColumnQuestions}`
+              `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='physics' ORDER BY RANDOM() LIMIT ${matchColumnQuestions}`
             ).then((resp) => {
               if (resp.rows) phyQues.push(resp.rows);
               const mcqQuestions = testObject.mcqdata
                 ? testObject.mcqdata[0]
                 : 0;
               db.query(
-                `SELECT * FROM questions WHERE is_reported=FALSE OR subject='chemistry' ORDER BY RANDOM() LIMIT ${mcqQuestions}`
+                `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='chemistry' ORDER BY RANDOM() LIMIT ${mcqQuestions}`
               ).then((resp) => {
                 if (resp.rows) chemQues.push(resp.rows);
                 const fibQuestions = testObject.fibdata
                   ? testObject.fibdata[0]
                   : 0;
                 db.query(
-                  `SELECT * FROM questions WHERE is_reported=FALSE OR subject='chemistry' ORDER BY RANDOM() LIMIT ${fibQuestions}`
+                  `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='chemistry' ORDER BY RANDOM() LIMIT ${fibQuestions}`
                 ).then((resp) => {
                   if (resp.rows) chemQues.push(resp.rows);
                   const assertionQuestions = testObject.assertiondata
                     ? testObject.assertiondata[0]
                     : 0;
                   db.query(
-                    `SELECT * FROM questions WHERE is_reported=FALSE OR subject='chemistry' ORDER BY RANDOM() LIMIT ${assertionQuestions}`
+                    `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='chemistry' ORDER BY RANDOM() LIMIT ${assertionQuestions}`
                   ).then((resp) => {
                     if (resp.rows) chemQues.push(resp.rows);
                     const trueFalseQuestions = testObject.truefalse
                       ? testObject.truefalse[0]
                       : 0;
                     db.query(
-                      `SELECT * FROM questions WHERE is_reported=FALSE OR subject='chemistry' ORDER BY RANDOM() LIMIT ${trueFalseQuestions}`
+                      `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='chemistry' ORDER BY RANDOM() LIMIT ${trueFalseQuestions}`
                     ).then((resp) => {
                       if (resp.rows) chemQues.push(resp.rows);
                       const numericalQuestions = testObject.numerical
                         ? testObject.numerical[0]
                         : 0;
                       db.query(
-                        `SELECT * FROM questions WHERE is_reported=FALSE OR subject='chemistry' ORDER BY RANDOM() LIMIT ${numericalQuestions}`
+                        `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='chemistry' ORDER BY RANDOM() LIMIT ${numericalQuestions}`
                       ).then((resp) => {
                         if (resp.rows) chemQues.push(resp.rows);
                         const matchColumnQuestions = testObject.matchcolumn
                           ? testObject.matchcolumn[0]
                           : 0;
                         db.query(
-                          `SELECT * FROM questions WHERE is_reported=FALSE OR subject='chemistry' ORDER BY RANDOM() LIMIT ${matchColumnQuestions}`
+                          `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='chemistry' ORDER BY RANDOM() LIMIT ${matchColumnQuestions}`
                         ).then((resp) => {
                           if (resp.rows) chemQues.push(resp.rows);
                           const mcqQuestions = testObject.mcqdata
                             ? testObject.mcqdata[0]
                             : 0;
                           db.query(
-                            `SELECT * FROM questions WHERE is_reported=FALSE OR subject='maths' ORDER BY RANDOM() LIMIT ${mcqQuestions}`
+                            `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='maths' ORDER BY RANDOM() LIMIT ${mcqQuestions}`
                           ).then((resp) => {
                             if (resp.rows) mathQues.push(resp.rows);
                             const fibQuestions = testObject.fibdata
                               ? testObject.fibdata[0]
                               : 0;
                             db.query(
-                              `SELECT * FROM questions WHERE is_reported=FALSE OR subject='maths' ORDER BY RANDOM() LIMIT ${fibQuestions}`
+                              `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='maths' ORDER BY RANDOM() LIMIT ${fibQuestions}`
                             ).then((resp) => {
                               if (resp.rows) mathQues.push(resp.rows);
                               const assertionQuestions = testObject.assertiondata
                                 ? testObject.assertiondata[0]
                                 : 0;
                               db.query(
-                                `SELECT * FROM questions WHERE is_reported=FALSE OR subject='maths' ORDER BY RANDOM() LIMIT ${assertionQuestions}`
+                                `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='maths' ORDER BY RANDOM() LIMIT ${assertionQuestions}`
                               ).then((resp) => {
                                 if (resp.rows) mathQues.push(resp.rows);
                                 const trueFalseQuestions = testObject.truefalse
                                   ? testObject.truefalse[0]
                                   : 0;
                                 db.query(
-                                  `SELECT * FROM questions WHERE is_reported=FALSE OR subject='maths' ORDER BY RANDOM() LIMIT ${trueFalseQuestions}`
+                                  `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='maths' ORDER BY RANDOM() LIMIT ${trueFalseQuestions}`
                                 ).then((resp) => {
                                   if (resp.rows) mathQues.push(resp.rows);
                                   const numericalQuestions = testObject.numerical
                                     ? testObject.numerical[0]
                                     : 0;
                                   db.query(
-                                    `SELECT * FROM questions WHERE is_reported=FALSE OR subject='maths' ORDER BY RANDOM() LIMIT ${numericalQuestions}`
+                                    `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='maths' ORDER BY RANDOM() LIMIT ${numericalQuestions}`
                                   ).then((resp) => {
                                     if (resp.rows) mathQues.push(resp.rows);
                                     const matchColumnQuestions = testObject.matchcolumn
                                       ? testObject.matchcolumn[0]
                                       : 0;
                                     db.query(
-                                      `SELECT * FROM questions WHERE is_reported=FALSE OR subject='maths' ORDER BY RANDOM() LIMIT ${matchColumnQuestions}`
+                                      `SELECT qid,statement,img_path,type,archive,latex,options FROM questions WHERE is_reported=FALSE OR subject='maths' ORDER BY RANDOM() LIMIT ${matchColumnQuestions}`
                                     ).then((resp) => {
                                       if (resp.rows) mathQues.push(resp.rows);
                                       res.json({
